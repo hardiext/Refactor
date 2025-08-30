33dbea9cc37d6fa6686d432503a5fa14de7ecde6
hardiektatendra <hardiektatendta@gmail.com>
2025-08-31 03:43:25 +0700

    add z-index

--- src/app/(routes)/components/molecules/bottom-nav.tsx
+++ src/app/(routes)/components/molecules/bottom-nav.tsx
@@ -22,7 +22,7 @@ export default function BottomNav() {
   const pathname = usePathname();
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-md lg:hidden">
+    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-md lg:hidden z-20">
       <ul className="flex justify-around max-w-lg mx-auto py-2">
         {navItems.map(({ label, href, icon: Icon }) => {
           const isActive = pathname === href;
